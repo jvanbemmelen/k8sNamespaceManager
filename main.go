296765f9473dc8b3c2f86e2bc4f0d6a40fa27549
Jan van Bemmelen <jan@tokyoeye.net>
2018-09-16 18:42:22 +0200

    create namespace now works with resources

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -15,66 +12,24 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 
-	jwtmiddleware "github.com/auth0/go-jwt-middleware"
-	jwt "github.com/dgrijalva/jwt-go"
-
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-type Response struct {
-	Message string `json:"message"`
-}
-
-type Jwks struct {
-	Keys []JSONWebKeys `json:"keys"`
-}
-
-type JSONWebKeys struct {
-	Kty string   `json:"kty"`
-	Kid string   `json:"kid"`
-	Use string   `json:"use"`
-	N   string   `json:"n"`
-	E   string   `json:"e"`
-	X5c []string `json:"x5c"`
-}
 
 type k8s struct {
 	clientset kubernetes.Interface
 }
 
 var (
-	kubernetesConfig = "/home/jvanbemmelen/.kube/config"
+	// default resourceQuota limits
+	hardPods           = resource.NewQuantity(5, resource.DecimalSI)
+	hardRequestsCPU    = resource.NewQuantity(1, resource.DecimalSI)
+	hardRequestsMemory = resource.NewQuantity(1*1024*1024*1024, resource.BinarySI)
+	hardLimitsCPU      = resource.NewQuantity(5, resource.DecimalSI)
+	hardLimitsMemory   = resource.NewQuantity(5*1024*1024*1024, resource.BinarySI)
 )
 
-var jwtMiddleWare *jwtmiddleware.JWTMiddleware
-
 func main() {
-	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
-		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-			aud := os.Getenv("AUTH0_API_AUDIENCE")
-			checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
-			if !checkAudience {
-				return token, errors.New("Invalid audience.")
-			}
-			// verify iss claim
-			iss := os.Getenv("AUTH0_DOMAIN")
-			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
-			if !checkIss {
-				return token, errors.New("Invalid issuer.")
-			}
-
-			cert, err := getPemCert(token)
-			if err != nil {
-				log.Fatalf("could not get cert: %+v", err)
-			}
-
-			result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
-			return result, nil
-		},
-		SigningMethod: jwt.SigningMethodRS256,
-	})
-
-	jwtMiddleWare = jwtMiddleware
 	// setup gin router
 	router := gin.Default()
 
@@ -87,58 +42,13 @@ func main() {
 				"message": "pong",
 			})
 		})
-		api.GET("/list", authMiddleware(), ListNamespacesHandler)
-		api.POST("/create/:namespace", authMiddleware(), CreateNamespaceHandler)
+		api.GET("/list", ListNamespacesHandler)
+		api.POST("/create/:namespace", CreateNamespaceHandler)
+		api.GET("/status/:namespace", StatusNamespaceHandler)
 	}
 
 	// start this thing!
 	router.Run(":3000")
-}
-
-// getPemCert
-func getPemCert(token *jwt.Token) (string, error) {
-	cert := ""
-	resp, err := http.Get(os.Getenv("AUTH0_DOMAIN") + ".well-known/jwks.json")
-	if err != nil {
-		return cert, err
-	}
-	defer resp.Body.Close()
-
-	var jwks = Jwks{}
-	err = json.NewDecoder(resp.Body).Decode(&jwks)
-
-	if err != nil {
-		return cert, err
-	}
-
-	x5c := jwks.Keys[0].X5c
-	for k, v := range x5c {
-		if token.Header["kid"] == jwks.Keys[k].Kid {
-			cert = "-----BEGIN CERTIFICATE-----\n" + v + "\n-----END CERTIFICATE-----"
-		}
-	}
-
-	if cert == "" {
-		return cert, errors.New("unable to find appropriate key")
-	}
-
-	return cert, nil
-}
-
-// authMiddleware intercepts the requests, and check for a valid jwt token
-func authMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// Get the client secret key
-		err := jwtMiddleWare.CheckJWT(c.Writer, c.Request)
-		if err != nil {
-			// Token not found
-			fmt.Println(err)
-			c.Abort()
-			c.Writer.WriteHeader(http.StatusUnauthorized)
-			c.Writer.Write([]byte("Unauthorized"))
-			return
-		}
-	}
 }
 
 // CreateKubernetesClient creates kubernetes client
@@ -150,12 +60,14 @@ func CreateKubernetesClient() (*k8s, error) {
 
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create Kubernetes configuration: %v\n", err)
 		return nil, err
 	}
 
 	client := k8s{}
 	client.clientset, err = kubernetes.NewForConfig(config)
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create Kubernetes configuration: %v\n", err)
 		return nil, err
 	}
 
@@ -166,13 +78,14 @@ func CreateKubernetesClient() (*k8s, error) {
 func NameSpaceExists(namespace string) bool {
 	kubernetesAPI, err := CreateKubernetesClient()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "Failed to create Kubernetes client: %v\n", err)
 	}
 
 	getOptions := metav1.GetOptions{}
 
 	_, err = kubernetesAPI.clientset.CoreV1().Namespaces().Get(namespace, getOptions)
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to retriev namespace: %v\n", err)
 		return false
 	}
 
@@ -185,13 +98,13 @@ func ListNamespacesHandler(c *gin.Context) {
 
 	kubernetesAPI, err := CreateKubernetesClient()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "Failed to create Kubernetes client: %v\n", err)
 	}
 
 	listOptions := metav1.ListOptions{}
 	namespaces, err := kubernetesAPI.clientset.CoreV1().Namespaces().List(listOptions)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "Failed to retrieve namespaces: %v\n", err)
 	}
 
 	c.JSON(http.StatusOK, gin.H{
@@ -199,26 +112,86 @@ func ListNamespacesHandler(c *gin.Context) {
 	})
 }
 
-// CreateNamespaceHandler returns the requested namespaces
+// CreateNamespaceHandler creates a namespace with the default resource quota
 func CreateNamespaceHandler(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
 
 	// take the namespace parameter
 	namespace := c.Param("namespace")
 
-	kubernetesAPI, err := CreateKubernetesClient()
-	if err != nil {
-		log.Fatal(err)
-	}
+	message := ""
 
-	namespaceSpec := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}
+	if NameSpaceExists(namespace) {
+		message = "Namespace already exists"
+	} else {
+		const (
+			ResourcePods           v1.ResourceName = "pods"
+			ResourceRequestsCPU    v1.ResourceName = "requests.cpu"
+			ResourceRequestsMemory v1.ResourceName = "requests.memory"
+			ResourceLimitsCPU      v1.ResourceName = "limits.cpu"
+			ResourceLimitsMemory   v1.ResourceName = "limits.memory"
+		)
 
-	_, err = kubernetesAPI.clientset.CoreV1().Namespaces().Create(namespaceSpec)
-	if err != nil {
-		log.Fatal(err)
+		kubernetesAPI, err := CreateKubernetesClient()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create Kubernetes client: %v\n", err)
+		}
+
+		// create Namespace
+		namespaceConfiguration := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}
+		_, err = kubernetesAPI.clientset.CoreV1().Namespaces().Create(namespaceConfiguration)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create Kubernetes namespace: %v\n", err)
+		}
+
+		// create ResourceQuota
+		resourceQuotaConfiguration := &v1.ResourceQuota{
+			ObjectMeta: metav1.ObjectMeta{Name: namespace},
+			Spec: v1.ResourceQuotaSpec{Hard: v1.ResourceList{ResourcePods: *hardPods,
+				ResourceRequestsCPU:    *hardRequestsCPU,
+				ResourceRequestsMemory: *hardRequestsMemory,
+				ResourceLimitsCPU:      *hardLimitsCPU,
+				ResourceLimitsMemory:   *hardLimitsMemory}}}
+		_, err = kubernetesAPI.clientset.CoreV1().ResourceQuotas(namespace).Create(resourceQuotaConfiguration)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create Kubernetes resourceQuota: %v\n", err)
+		}
+		message = "Namespace created"
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"message": "created",
+		"message": message,
+	})
+}
+
+// StatusNamespaceHandler creates a namespace with the default resource quota
+func StatusNamespaceHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+
+	// take the namespace parameter
+	namespace := c.Param("namespace")
+
+	message := ""
+
+	if !NameSpaceExists(namespace) {
+		message = "Namespace does not exists"
+	} else {
+		kubernetesAPI, err := CreateKubernetesClient()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create Kubernetes client: %v\n", err)
+		}
+
+		// get resourceQuota
+		getOptions := metav1.GetOptions{}
+		namespaceQuota := ""
+		namespaceQuota, err = kubernetesAPI.clientset.CoreV1().ResourceQuotas(namespace).Get(namespace, getOptions)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create Kubernetes resourceQuota: %v\n", err)
+		}
+		message = namespaceQuota
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": message,
 	})
 }
